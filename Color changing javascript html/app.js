35f9f4c98af4340e0334f0b8c78ca178e323197b
Olek <94283587+olek-program@users.noreply.github.com>
2022-12-31 12:24:49 +0100

    d
    d

---
+++ Color changing javascript html/app.js
@@ -0,0 +1,16 @@
+const greenButton = document.querySelector("green")
+const blueButton = document.querySelector("blue")
+const redButton = document.querySelector("red")
+const div = document.querySelector("div")
+
+function greenButtonColorChange() {
+    div.style.borderColor = "green"
+}
+
+function blueButtonColorChange() {
+    div.style.borderColor = "blue"
+}
+
+function redButtonColorChange() {
+    div.style.borderColor = "red"
+}
